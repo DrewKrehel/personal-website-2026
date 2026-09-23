e7f103a9764af81bca0fe5c6958e46faf92b8f0a
Drew Krehel <drewkrehel@gmail.com>
2026-09-22 20:00:28 -0500

    imported models to admin and set up superuser

--- builder/admin.py
+++ builder/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from builder.models import Profile, Project, Resume, Link, Form, FormSubmission
+
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Resume)
+admin.site.register(Link)
+admin.site.register(Form)
+admin.site.register(FormSubmission)
+
