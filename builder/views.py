df6fec8313f796d08ab418a5839bc461003dfdea
Drew Krehel <drewkrehel@gmail.com>
2026-09-23 20:10:45 -0500

    filled in index and created about_me page. Working on profile_image

--- builder/views.py
+++ builder/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Profile
 
 def home(request):
+    profile = Profile.objects.get(user__username="drewkrehel")
+    
+    context = {
+        'profile': profile,
+    }
+    
+    return render(request, 'index.html', context=context)
 
-    return render(request, 'index.html')
+def about_me(request):
+    profile = Profile.objects.get(user__username="drewkrehel")
+    
+    context = {
+        'profile': profile,
+    }
+    
+    return render(request, 'about_me.html', context=context)
